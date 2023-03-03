526e1c95dca34eeeb6784df95e7dee684ba95550
cubdesign <takeo@cubdesign.com>
2023-03-03 19:42:50 +0900

    workspace setring

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # todos-ts-graphql
+
+## 参考
+
+【TypeScript & GraphQL】で Fullstack Web Application開発
+https://www.youtube.com/watch?v=er4-IgfZk68&list=PLOvyErVZBqQNUVbvY6s5dAHy2XLkBC7oc&ab_channel=JuniorDevMind
+
+サンプルコード
+https://github.com/ryuichi24/ts-fullstack-gql
---
+++ packages/frontend/.eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["tsconfig.eslint.json"],
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {},
+};
